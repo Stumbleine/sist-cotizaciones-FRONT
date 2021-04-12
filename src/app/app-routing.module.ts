eeea38c3e52a1fc1b340c189f3a388609bf43256
Stumbleine <48832487+Stumbleine@users.noreply.github.com>
2021-04-12 17:54:00 -0600

    corrigiendo error de ruta "user-rug"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './rugUser/home-page/home-page.component';
+import { HomePageComponent } from './userRUG/home-page/home-page.component';
 const routes: Routes = [{ path: 'home-rug', component:  HomePageComponent},];
 
 @NgModule({
