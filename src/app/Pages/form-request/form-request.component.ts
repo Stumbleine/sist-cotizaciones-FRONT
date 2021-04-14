7218ed8300f97b3f5fb5685e4d798fc77583118d
jheimc <jheimymiranda@gmail.com>
2021-04-13 20:08:55 -0400

    validacion de campos en el formulario

--- src/app/Pages/form-request/form-request.component.ts
+++ src/app/Pages/form-request/form-request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 export interface PeriodicElement {
   name: string;
@@ -15,18 +16,64 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
   
 ];
-
+interface Product{
+  product: string;
+  cant: Number;
+  unit: string;
+  detail:string;
+}
+interface Request{
+  date: Date;
+  sigla: string;
+  total: number;
+  justify: string;
+}
+const listProducts: Product[] = [];
+var i=0;
 @Component({
   selector: 'app-form-request',
   templateUrl: './form-request.component.html',
   styleUrls: ['./form-request.component.css']
 })
 export class FormRequestComponent implements OnInit {
-
-  constructor() { }
+  productForm = this.formBuilder.group({
+    product: ['',[Validators.required]],
+    cant: ['',[Validators.required]],
+    unit: ['',[Validators.required]],
+    detail: ['',[Validators.required]],
+  });
+  requestForm = this.formBuilder.group({
+    date: ['',],
+    sigla: ['',],
+    total: ['',[Validators.required]],
+    justify: ['',[Validators.required]],
+  });
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
+  
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
+  pressed:boolean;
+
+  saveProduct(value){
+      console.log(JSON.stringify(value))
+      this.pressed=true;
+      listProducts[i]=value;
+      i++;
+      console.log(listProducts);
+      //this.productForm.reset();
+    }
+    
+  saveProductPressed():boolean{
+    return this.pressed;
+  }
+  saveRequest(request){
+    console.log(JSON.stringify(request))
+    
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,7 +22,8 @@ import {MatTableModule} from '@angular/material/table';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatTableModule
+    MatTableModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent,FormRequestComponent]
