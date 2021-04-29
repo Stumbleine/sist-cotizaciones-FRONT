e921adbc5ab9bff09871db3c1381d585c9919ec0
Stumbleine <48832487+Stumbleine@users.noreply.github.com>
2021-04-29 13:15:59 -0600

    seccion2 de form-base

--- src/app/userRAF/form-quotation/form-quotation.component.ts
+++ src/app/userRAF/form-quotation/form-quotation.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+
+export interface Items{
+  idRequestDetail: number;
+  quantity: number;
+  unit: string;
+  description:string;
+}
+const  Items=[
+  {quantity: 1, unit: 'Hydrogen', description: 1.0079},
+  {quantity: 1, unit: 'Hydrogen', description: 1.0079},
+  {quantity: 1, unit: 'Hydrogen', description: 1.0079},
+  {quantity: 1, unit: 'Hydrogen', description: 1.0079},
+];
 
 @Component({
   selector: 'app-form-quotation',
@@ -6,8 +21,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-quotation.component.css']
 })
 export class FormQuotationComponent implements OnInit {
-
   constructor() { }
+
+  displayedColumns: string[] = ['index', 'quantity', 'unit', 'description','unitPrice','subtotalPrice'];
+  dataSource =  new MatTableDataSource<Items>([]);
+  columnas=[
+    
+    {titulo:"CANTIDAD" ,name: "quantity"},
+    {titulo:"UNIDAD" ,name: "unit"},
+    {titulo:"DETALLE" ,name: "description"},
+    {titulo:"P. UNITARIO" ,name: "unitPrice"},
+    {titulo:"SUBTOTAL" ,name: "subtotalPrice"}
+  ];
+  public items:Items[]=[];
+  
 
   ngOnInit(): void {
   }
