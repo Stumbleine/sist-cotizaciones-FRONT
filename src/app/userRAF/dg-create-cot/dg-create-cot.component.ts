ef279e336260cce36daf38d4d3d6eec8a8eee6e1
Stumbleine <48832487+Stumbleine@users.noreply.github.com>
2021-05-03 22:10:47 -0600

    casi termino

--- src/app/userRAF/dg-create-cot/dg-create-cot.component.ts
+++ src/app/userRAF/dg-create-cot/dg-create-cot.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {RequestService} from '../../services/request.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-dg-create-cot',
   templateUrl: './dg-create-cot.component.html',
@@ -9,14 +11,27 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 export class DgCreateCotComponent implements OnInit {
   favoriteSeason: string;
   seasons: string[] = ['Cotizacion manual', 'Cotizacion digital'];
+  quotationForm={idSpendingRequest:this.data.idSR};
 
   constructor(
-    public dialogRef: MatDialogRef<DgCreateCotComponent>) {}
+    public dialogRef: MatDialogRef<DgCreateCotComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private RequestService: RequestService) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
   ngOnInit(): void {
+    console.log("Este es el id de la solicitud:",this.quotationForm);
+  }
+  createQuataion(){
+    this.RequestService.post('http://localhost:8080/api/quotation',null)
+    .subscribe(r =>{
+      console.log('Solicitud enviada!!');
+
+    })
+
+
   }
 
 }
