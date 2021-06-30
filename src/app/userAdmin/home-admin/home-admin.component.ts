8676487371dc51fb027d640648fa1c5e39270c94
Stumbleine <48832487+Stumbleine@users.noreply.github.com>
2021-06-30 04:56:18 -0400

    terminado

--- src/app/userAdmin/home-admin/home-admin.component.ts
+++ src/app/userAdmin/home-admin/home-admin.component.ts
@@ -3,10 +3,16 @@ import { MatDialog } from '@angular/material/dialog';
 import { RegisterUnidadComponent } from '../register-unidad/register-unidad.component';
 import { RegisterRoleComponent } from '../register-role/register-role.component';
 import { RegisterUserComponent } from '../register-user/register-user.component';
-
+import { MatTableDataSource } from '@angular/material/table';
 import {RequestService} from '../../services/request.service';
 
-
+export interface User{
+  idUser: number;
+  name: string;
+  privileges: string[];
+  role:string;
+  spendingUnit:string;
+}
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
@@ -16,21 +22,36 @@ export class HomeAdminComponent implements OnInit {
 
   constructor(public dialog: MatDialog, private RequestService: RequestService,) { }
   units:any;
-  users:any
+  usersResponse:any;
+  users:User[]=[];
+
+  displayedColumns: string[] = ['index', 'name', 'role', 'privileges','spendingUnit','actions'];
+  dataSource =  new MatTableDataSource<User>([]);
+  columnas=[
+    {titulo:"NOMBRE USUARIO" ,name: "name"},
+    {titulo:"USER ROL" ,name: "role"},
+    {titulo:"PRIVILEGIOS" ,name: "privileges"},
+    {titulo:"UNIDAD" ,name: "spendingUnit"},
+
+  ];
+
   ngOnInit(): void {
     this.loadUnits();
+    this.loadUsers();
   }
 
   //localhost:8080/api/user/allUsers
   loadUnits(){
-    this.RequestService.get('http://localhost:8080/api/spendingUnit/allSpendingUnits').subscribe(r=>{
+    this.RequestService.get('http://localhost:8080/api/spendingUnit/allSpendingUnits ').subscribe(r=>{
       this.units=r;
       console.log(this.units)
     })
   }
   loadUsers(){
     this.RequestService.get('http://localhost:8080/api/user/allUsers').subscribe(r=>{
-      this.users=r;
+      this.usersResponse=r;
+      this.users=this.usersResponse;
+      this.dataSource.data=this.users;
       console.log(this.users)
     })
   }
