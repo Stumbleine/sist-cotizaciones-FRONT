47015ffbfe689f00a3b7e6bff29c50128054ebe8
Stumbleine <48832487+Stumbleine@users.noreply.github.com>
2021-06-30 03:42:11 -0400

    unit get

--- src/app/userAdmin/home-admin/home-admin.component.ts
+++ src/app/userAdmin/home-admin/home-admin.component.ts
@@ -3,6 +3,10 @@ import { MatDialog } from '@angular/material/dialog';
 import { RegisterUnidadComponent } from '../register-unidad/register-unidad.component';
 import { RegisterRoleComponent } from '../register-role/register-role.component';
 import { RegisterUserComponent } from '../register-user/register-user.component';
+
+import {RequestService} from '../../services/request.service';
+
+
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
@@ -10,11 +14,31 @@ import { RegisterUserComponent } from '../register-user/register-user.component'
 })
 export class HomeAdminComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
-
-  
+  constructor(public dialog: MatDialog, private RequestService: RequestService,) { }
+  units:any;
+  users:any
   ngOnInit(): void {
+    this.loadUnits();
   }
+
+  //localhost:8080/api/user/allUsers
+  loadUnits(){
+    this.RequestService.get('http://localhost:8080/api/spendingUnit/allSpendingUnits').subscribe(r=>{
+      this.units=r;
+      console.log(this.units)
+    })
+  }
+  loadUsers(){
+    this.RequestService.get('http://localhost:8080/api/user/allUsers').subscribe(r=>{
+      this.users=r;
+      console.log(this.users)
+    })
+  }
+
+
+
+
+
 
   openRegisterUnidad(){
     this.dialog.open(RegisterUnidadComponent
