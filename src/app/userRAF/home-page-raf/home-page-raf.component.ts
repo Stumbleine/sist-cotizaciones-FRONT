2649dd8adf5b9292816babb37bea0d92b0cc1e0b
jheimc <jheimymiranda@gmail.com>
2021-07-13 04:18:37 -0400

    Filtrar terminado

--- src/app/userRAF/home-page-raf/home-page-raf.component.ts
+++ src/app/userRAF/home-page-raf/home-page-raf.component.ts
@@ -9,15 +9,31 @@ export class HomePageRAFComponent implements OnInit {
   message="admin";
   constructor(private RequestService: RequestService) { }
   public requestsReceived=null;
-  
+  public requestsReceivedCopy=null
+  public status= "";
+  public notRequest=false;
+
   ngOnInit(): void {
     this.loadData();
   }
   loadData(){
-    this.RequestService.get('http://localhost:8080/api/administration')
+    this.RequestService.get('http://localhost:8080/api/spendingUnit')
     .subscribe(r=>{
       console.log(r);
       this.requestsReceived = r;
+      this.requestsReceivedCopy=r;
     })
+  }
+  filterBy(option){
+    this.notRequest=false;
+    this.status=option;
+    if(this.status=="Todos" || this.status==""){
+      this.requestsReceived=this.requestsReceivedCopy
+    }else{
+      this.requestsReceived=this.requestsReceivedCopy.filter(request => request.status=== this.status);
+      if(this.requestsReceived.length==0){
+        this.notRequest=true;
+      }
+    }
   }
 }
--- src/app/userRUG/home-page/home-page.component.ts
+++ src/app/userRUG/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import {RequestService} from '../../services/request.service';
 @Component({
   selector: 'app-home-page',
@@ -8,6 +9,9 @@ import {RequestService} from '../../services/request.service';
 export class HomePageComponent implements OnInit {
   constructor(private RequestService: RequestService) { }
   public requestsSent=null
+  public requestsSentCopy=null
+  public status= "";
+  public notRequest=false;
 
   ngOnInit(): void {
     this.loadData();
@@ -15,8 +19,21 @@ export class HomePageComponent implements OnInit {
   loadData(){
     this.RequestService.get('http://localhost:8080/api/spendingUnit/1')
     .subscribe(r=>{
-      console.log(r);
       this.requestsSent = r;
+      this.requestsSentCopy=r;
+      
     })
+  }
+  filterBy(option){
+    this.notRequest=false;
+    this.status=option;
+    if(this.status=="Todos" || this.status==""){
+      this.requestsSent=this.requestsSentCopy
+    }else{
+      this.requestsSent=this.requestsSentCopy.filter(request => request.status=== this.status);
+      if(this.requestsSent.length==0){
+        this.notRequest=true;
+      }
+    }
   }
 }
